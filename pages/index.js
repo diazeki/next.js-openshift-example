4cb6f194910079612cf6cd20a9cb7649434fa5dd
Dia Azzawi <dia.azzawi@perficient.com>
2024-07-12 09:47:36 -0500

    Changed title of main page for the third time

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome dude to my <a href="https://nextjs.org">Next.js app!</a>
         </h1>
 
         <p className={styles.description}>
